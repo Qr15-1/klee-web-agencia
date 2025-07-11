8d6326545e4675e227d5f80971df48fbe5be5ada
Roger Alejandro Quijada Avilera <rogeralejandroquijadaavilera@.com>
2025-07-11 08:34:06 -0400

    fix: importar iconos mdi y heroicons desde JSON para compatibilidad con Render

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import mdi from "@iconify-json/mdi/icons.json";
+import heroicons from "@iconify-json/heroicons/icons.json";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,7 +10,10 @@ export default defineConfig({
   integrations: [
     tailwind(),
     icon({
-      sets: { mdi },
+      sets: {
+        mdi,
+        heroicons,
+      },
     }),
   ],
 
