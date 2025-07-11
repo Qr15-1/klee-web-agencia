e9d8cc7f3ecdce5e37fc02946be4ab58245c3eb3
Roger Alejandro Quijada Avilera <rogeralejandroquijadaavilera@.com>
2025-07-11 00:50:19 -0400

    fix: Configurar astro-icon para incluir conjunto 'mdi'

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,24 +4,28 @@ import icon from "astro-icon";
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 
-// Obtenemos la ruta base del proyecto
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [
+    tailwind(), 
+    // --- CAMBIO AQUÍ ---
+    icon({ 
+      include: ['mdi'] // Le decimos a astro-icon que incluya el conjunto de iconos 'mdi'
+    })
+    // --- FIN CAMBIO ---
+  ],
 
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
   },
 
-  // --- NUEVA CONFIGURACIÓN DE ALIAS AÑADIDA ---
   vite: {
     resolve: {
       alias: {
-        // Le decimos a Vite/Astro que '@' es un atajo para la carpeta 'src'
         '@': resolve(__dirname, 'src'),
       }
     }
